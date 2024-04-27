1c4568d84c2b04fa23afa6b570d9e35481feaa1c
Callum Fortune <callumfortune03@outlook.com>
2024-04-27 01:14:37 +0100

    two 256 bit primes generated

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,40 @@
-use num_bigint::{BigUint, RandBigInt};
-use rand::prelude::*;
+use num_bigint::{BigInt, RandBigInt};
+use rand;
+use rust_miller_rabin::miller_rabin::miller_rabin;
 
-fn generate_key_pair() {
+fn generate_prime(bit_size: u64) -> BigInt {
+
+    let mut rng = rand::thread_rng();
+
+    let mut result: BigInt = BigInt::from(0);
+
+    loop {
+        let potential_prime = rng.gen_bigint(bit_size);
+        if miller_rabin(&potential_prime){
+            result = potential_prime;
+            break;
+        }
+
+    }
+
+    return result;
+
+}
+
+fn generate_key_pair() -> (BigInt, BigInt) {
+
+    let prime_bit_size = 256;
+
+    let prime_one = generate_prime(prime_bit_size);
+    let prime_two = generate_prime(prime_bit_size);
+
+    return (prime_one, prime_two)
+
+    // let prime = confirmed_prime;
+
+    // println!("{}", prime);
+
+    // return ((prime, confirmed_prime), (confirmed_prime, confirmed_prime));
 
 }
 
@@ -15,10 +48,6 @@ fn decrypt() {
 
 fn main() {
 
-    let mut rng = rand::thread_rng();
-
-    let rand = rng.gen_bigint(4096);
-
-    println!("{}", rand);
+    println!("result: {:?}", generate_key_pair())
 
 }
