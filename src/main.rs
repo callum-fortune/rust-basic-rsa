c9ae9ad30666b0edae1f24576022851bf76be019
Callum Fortune <callumfortune03@outlook.com>
2024-04-27 01:44:16 +0100

    generates totient and finds its coprime

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,15 @@
+use num_traits::{Zero, One};
 use num_bigint::{BigInt, RandBigInt};
 use rand;
 use rust_miller_rabin::miller_rabin::miller_rabin;
+
+fn gcd(num_one: BigInt, num_two: BigInt) -> BigInt {
+    if num_one.is_zero() {
+        return num_two;
+    }
+    return gcd(num_two.clone() % num_one.clone(), num_one);
+}
+
 
 fn generate_prime(bit_size: u64) -> BigInt {
 
@@ -21,20 +30,34 @@ fn generate_prime(bit_size: u64) -> BigInt {
 
 }
 
-fn generate_key_pair() -> (BigInt, BigInt) {
+fn generate_key_pair() -> (BigInt, BigInt, BigInt) {
 
     let prime_bit_size = 256;
 
     let prime_one = generate_prime(prime_bit_size);
     let prime_two = generate_prime(prime_bit_size);
 
-    return (prime_one, prime_two)
+    let multiple_of_primes = &prime_one * &prime_two;
 
-    // let prime = confirmed_prime;
+    let totient = (&prime_one - BigInt::one()) * (&prime_two - BigInt::one());
 
-    // println!("{}", prime);
+    println!("totient {}", totient);
 
-    // return ((prime, confirmed_prime), (confirmed_prime, confirmed_prime));
+    let mut exponent = BigInt::from(0);
+
+    loop {
+        println!("expo loop");
+        let potential_exponent = generate_prime(20);
+
+        if gcd(potential_exponent.clone(), totient.clone()) == BigInt::one() {
+            exponent = potential_exponent;
+            break;
+        }
+    }
+
+
+
+    return (prime_one, prime_two, exponent)
 
 }
 
